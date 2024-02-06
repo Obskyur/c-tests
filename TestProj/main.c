63b653c1832cb8d33b61bf58458d1a9df5a4854d
Obskyur <parkourman.miller@gmail.com>
2024-02-05 20:33:47 -0800

    docs: Add comments to main.c

--- TestProj/main.c
+++ TestProj/main.c
@@ -2,8 +2,10 @@
 #include "quicksort.h"
 
 void main() {
+	// Play with colors:
 	runColors();
+
+	// Play with numbers:
 	int nums[] = { 8, 7, 4, 13, 12, 23, 2, 1, 5 };
 	runQuickSort(nums, sizeof(nums)/sizeof(nums[0]));
 }
-
